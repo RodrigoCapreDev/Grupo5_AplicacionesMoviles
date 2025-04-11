a2e01a79ef71a06c111784faf896a989614baee7
rcapredev <rodrigocapre@gmail.com>
2025-04-11 15:51:56 -0300

    Primer commit creacion de proyecto TP1
    Se empieza a trabajar con la consigna, se crean los componentes para el login

---
+++ TP1/app/src/main/java/com/grupo5/app1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.grupo5.app1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  // Esto conecta con tu layout
+    }
+}
